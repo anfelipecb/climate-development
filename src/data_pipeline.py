fd74104b0e57a71c618a845729cfd6ae8d074c60
Andrés F. Camacho <af.camacho.b9@gmail.com>
2025-11-04 23:55:58 -0600

    Static Viz Project with HTML page

---
+++ src/data_pipeline.py
@@ -0,0 +1,7 @@
+"""
+Pipeline to get run the data gathering in 
+1) From EDS - ERA5 API
+2) From the Stata file with the papers
+3) From model'ss regressions (exported from stata)
+"""
+
---
+++ src/generate_graphs.py
@@ -0,0 +1,6 @@
+"""
+Here we create the graphs that would be used in the HTML static story to 
+present the introduction to the papers
+"""
+
+# Firts graph, time series of temperature
